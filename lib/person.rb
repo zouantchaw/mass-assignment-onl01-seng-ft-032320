ddac77d979d73d68f29e2ce7a74e97bc2f3699c2
Wiel Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2020-04-09 16:06:51 -0400

    VS commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
 class Person
-  #your code here
+  attr_accesssor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
 end
