19a6a32e2372ae711306a5e31823bdfbcc9fa488
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 23:53:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,13 @@
 class Person
+<<<<<<< HEAD
   attr_accesssor :name, :birthday, :hair_color, :eye_color, :height, 
   :weight, :handed, :complexion, :t_shirt_size, 
   :wrist_size, :glove_size, :pant_length, :pant_width
+=======
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+
+>>>>>>> 8f4c392631ec019c460875eee183e81086a00d0a
 end
