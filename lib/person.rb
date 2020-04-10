27d2f66b1b0d881ba60184f4fa2d1ff37bf7c22f
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-09 22:00:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,9 @@
 class Person
-  attr_accesssor :name, :birthday, :hair_color, :eye_color, :height, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
   :weight, :handed, :complexion, :t_shirt_size, 
   :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end 
 end
